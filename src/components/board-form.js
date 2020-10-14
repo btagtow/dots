362df0b190f4e1ceff746a2aec886d4696426b76
Ben Tagtow <bentagtow@gmail.com>
2020-10-14 10:14:48 -0600

    initial commit - react template w/ header

---
+++ src/components/board-form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class boardForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
