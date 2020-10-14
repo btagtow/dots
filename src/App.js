678373635eeba51f287336de0c902a4b1f727d3a
Ben Tagtow <bentagtow@gmail.com>
2020-10-14 11:34:59 -0600

    controlled form for rows/columns in dots grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BoardForm from './components/board-form'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="header">DOTS</h1>
+      <BoardForm />
       
       <div className="main">
         <div className="board-box"></div> 
